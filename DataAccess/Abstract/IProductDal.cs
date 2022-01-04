d13060b2d42777f420c1d52097eb7eeaf95ac61e
musabalin@hotmail.com <musabalin@hotmail.com>
2022-01-04 09:49:29 +0300

    update1

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,13 +1,12 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace DataAccess.Abstract
 {
-    public class Category : IEntity
+    public interface IProductDal
     {
     }
 }
--- DataAccess/Concrete/ProductDal.cs
+++ DataAccess/Concrete/ProductDal.cs
@@ -1,13 +1,12 @@
-﻿using Core.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace DataAccess.Concrete
 {
-    public class Category : IEntity
+    public class ProductDal
     {
     }
 }
